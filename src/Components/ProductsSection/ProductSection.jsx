619002096b5f9e779eb8824a3fd5f088521f9761
Alireza Barkhordari <aliirezaa.barkhordarii@gmail.com>
2023-03-17 11:52:57 +0330

    made some responsive

--- src/Components/ProductsSection/ProductSection.jsx
+++ src/Components/ProductsSection/ProductSection.jsx
@@ -7,14 +7,14 @@ import Image6 from "../../Assets/Image6.png";
 
 const ProductSection = () => {
   return (
-    <div className="flex h-screen justify-start">
-      <div className="w-[20%] h-full object-contain">
+    <div className="flex flex-col lg:flex-row min-h-screen justify-start p-6 lg:p-0">
+      <div className="hidden lg:inline-block w-[20%] h-full object-contain">
         <img src={Image2} className="object-cover h-full" alt="" />
       </div>
       <div className="w-full flex flex-col justify-between items-start">
         <div className="h-[49%] flex justify-between items-start px-2">
-          <div className="w-full h-full flex items-center justify-start gap-7">
-            <div className="w-[35%]">
+          <div className="w-full h-full flex flex-col lg:flex-row items-center justify-start gap-7">
+            <div className="w-full lg:w-[35%] mt-5">
               <div className="w-[120px] h-[10px] bg-myGreen rounded-md"></div>
               <h1 className="text-[2.5rem]">محصولات</h1>
               <h3 className="text-myGreen text-[1.3rem] font-bold">
@@ -27,22 +27,22 @@ const ProductSection = () => {
                 به شرایط هر مشتری پیش از خریدشان مشاوره ای را برای انتخاب بهترین
                 گزینه ارائه میدهد.
               </p>
-              <button className="border-[1px] border-[#292929] w-[130px] h-[2.5rem] rounded-[15px] mt-2 hover:border-[#239e48] hover:text-[#239e48] transition-all duration-200">
+              <button className="border-[1px] border-[#292929] w-[130px] h-[2.5rem] rounded-[15px] mt-3 hover:border-[#239e48] hover:text-[#239e48] transition-all duration-200">
                 همه محصولات
               </button>
             </div>
-            <div className="w-[61%] relative flex justify-center">
+            <div className="w-full lg:w-[61%] relative flex justify-center mt-10 lg:mt-0">
               <img src={Image4} className="w-[900px] object-contain" alt="1" />
             </div>
           </div>
         </div>
 
-        <div className="w-full h-[49%] flex justify-between items-start">
+        <div className="w-full h-[49%] flex justify-between items-start mt-12 lg:mt-0">
           <div className="w-full h-full flex items-center justify-start gap-16">
-            <div className="w-[57%]">
+            <div className="w-full lg:w-[57%]">
               <img src={Image5} className="object-contain" alt="" />
             </div>
-            <div className="w-[43%] h-full flex flex-col justify-between relative">
+            <div className="hidden w-full lg:w-[43%] h-full lg:flex flex-col justify-between relative">
               <div className="w-[90%]">
                 <h1 className="text-[1.7rem] text-myGreen font-bold">
                   ظرفیت دوهزار عدد RV
--- src/Components/SliderBanner/SliderBanner.jsx
+++ src/Components/SliderBanner/SliderBanner.jsx
@@ -11,8 +11,8 @@ import Image3 from "../../Assets/Image3.png";
 
 const SliderBanner = () => {
   return (
-    <div className="w-full flex justify-center items-center">
-      <div className="w-[55%] h-[800px] flex justify-between items-center p-1">
+    <div className="w-full flex flex-col lg:flex-row justify-center items-center">
+      <div className="w-[100%] h-[550px] lg:h-[800px] lg:w-[55%] flex justify-center lg:justify-between items-center p-1 gap-9">
         <div className="w-[10%] flex flex-col justify-center items-center gap-4">
           <div class="border-[3px] border-[#292929] rounded-full w-[18px] h-[18px] cursor-pointer hover:border-[#239e48]"></div>
           <div class="border-[3px] border-[#292929] rounded-full w-[18px] h-[18px] cursor-pointer hover:border-[#239e48]"></div>
@@ -23,19 +23,19 @@ const SliderBanner = () => {
         <div className="h-[800px] flex flex-col justify-center items-end p-1">
           <h1 className="TitleBanner">AutoLumo</h1>
           <h1 className="Title2Banner">A2000Plus</h1>
-          <h1 className="text-[2.7rem] font-bold mt-3">
+          <h1 className="text-[2.1rem] lg:text-[2.7rem] font-bold mt-3">
             تشخیص سریع, درمان به موقع
           </h1>
           <div className="bg-myGreen text-white px-3 py-2 mt-5">
             ظرفیت بارگذاری بیست و چهار تست متفاوت به طور همزمان از امتیازات
-            دستگاهاتولومو
+            دستگاه آتولومو
           </div>
-          <button className="border-[1px] border-[#292929] w-[130px] h-[2.5rem] rounded-[15px] mt-6 hover:border-[#239e48] hover:text-[#239e48] transition-all duration-200">
+          <button className="border-[1px] border-[#292929] w-[130px] h-[2.5rem] rounded-[15px] mt-6 hover:border-[#239e48] hover:text-[#ffffff] transition-all duration-200">
             درباره محصول
           </button>
         </div>
       </div>
-      <div className="w-[45%]">
+      <div className="w-[100%] lg:w-[45%] flex justify-center items-center">
         <div className="w-[600px] h-[620px] flex items-center justify-center">
           <LazyLoadImage
             effect="blur"
